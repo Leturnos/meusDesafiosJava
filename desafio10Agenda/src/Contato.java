5229e1db03faa0eb048f7b6eecd55a869a26eae3
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-09-28 16:54:59 -0300

    desafio10Agenda completed

--- desafio10Agenda/src/Contato.java
+++ desafio10Agenda/src/Contato.java
@@ -1,28 +1,17 @@
+import java.util.ArrayList;
+
 public class Contato {
     private String nome;
     private String telefone;
     private String email;
 
     public Contato() {
-
     }
 
     public Contato(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
-    }
-
-    public void pesquisarContato () {
-
-    }
-
-    public void adicionarContato () {
-
-    }
-
-    public static void removerContato() {
-
     }
 
     public String getNome() {
@@ -47,5 +36,13 @@ public class Contato {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato -> " +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'';
     }
 }
--- desafio10Agenda/src/Main.java
+++ desafio10Agenda/src/Main.java
@@ -7,13 +7,12 @@
 //Permita pesquisar, adicionar e remover.
 
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        ArrayList<Objects> agenda = new ArrayList<Objects>();
+        ArrayList<Contato> contatos = new ArrayList<Contato>();
 
         while (true) {
             System.out.println("== Agenda Contatos ==");
@@ -25,25 +24,82 @@ public class Main {
             String input = ler.nextLine();
 
             if (input.equals("1")) {
-                System.out.println("Pesquisar contatos:");
-                String busca = ler.nextLine();
+                if (contatos.isEmpty()) {
+                    System.out.println("Lista vazia");
+                }
+                else{
+                    String inputPesquisa;
+                    while (true) {
+                        System.out.println("Pesquisar contatos: (0 - todos, 1 - busca, 2 - sair)");
+                        inputPesquisa = ler.nextLine();
+                        if (inputPesquisa.equals("0")){
+                            for (Contato contato : contatos){
+                                System.out.println(contato);
+                                System.out.println();
+                            }
+                            break;
+                        }
+                        else if (inputPesquisa.equals("1")) {
+                            System.out.println("Digite o nome do contato:");
+                            String busca = ler.nextLine();
+                            boolean flagEncontrado = false;
+                            for (Contato contato : contatos){
+                                if (contato.getNome().equals(busca)) {
+                                    System.out.println(contato);
+                                    System.out.println();
+                                    flagEncontrado = true;
+                                }
+                            }
+                            if (!flagEncontrado) {
+                                System.out.println("contato não encontrado");
+                            }
+                            break;
+                        }
+                        else if (inputPesquisa.equals("2")) {
+                            break;
+                        }
+                        else {
+                            System.out.println("Opção inválida, retornando...");
+                            continue;
+                        }
+                    }
+                }
             }
 
             else if (input.equals("2")) {
-                while (true) {
-                    System.out.println("== Adicionar produto ==");
-                    System.out.println("Digite o Nome do contato:");
-                    String nome = ler.nextLine();
-                    System.out.println("Digite o telefone:");
-                    String tel = ler.nextLine();
-                    System.out.println("Digite a email:");
-                    String email = ler.nextLine();
-                }
+                System.out.println("== Adicionar produto ==");
+                System.out.println("Digite o Nome do contato:");
+                String nome = ler.nextLine();
+                System.out.println("Digite o telefone:");
+                String tel = ler.nextLine();
+                System.out.println("Digite a email:");
+                String email = ler.nextLine();
+
+                Contato adicionarContato = new Contato(nome, tel, email);
+                contatos.add(adicionarContato);
             }
             else if (input.equals("3")) {
-                Contato.removerContato();
+                if (contatos.isEmpty()) {
+                    System.out.println("Lista vazia");
+                }
+                else {
+                    System.out.println("Digite o Nome do contato a ser removido:");
+                    String nomeRemocao = ler.nextLine();
+                    boolean flagEncontrado = false;
+                    for (int i = 0; contatos.size() > i; i++){
+                        if (contatos.get(i).getNome().equals(nomeRemocao)) {
+                            System.out.println(contatos.get(i) + " :removido");
+                            contatos.remove(i);
+                            flagEncontrado = true;
+                        }
+                    }
+                    if (!flagEncontrado) {
+                        System.out.println("contato não encontrado");
+                    }
+                }
             }
             else if (input.equals("4")) {
+                System.out.println("Encerrando serviço...");
                 break;
             }
             else {
